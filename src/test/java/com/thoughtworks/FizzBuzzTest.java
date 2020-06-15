0587764c8c83276be08004e4ecc8eb34c1def436
humenglin <humenglin@zyb.com.cn>
2020-06-15 21:37:10 +0800

    [refactor]:重构测试代码，抽取公共方法

--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -7,169 +7,82 @@ import static org.junit.Assert.assertEquals;
 public class FizzBuzzTest {
     @Test
     public void should_return_1_when_say_given_1() {
-        // given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        // when
-        String resultActual = fizzBuzz.say(1);
-
-        // then
-        assertEquals("1", resultActual);
+        should_return_when_say_given_number(1, "1");
     }
 
     @Test
     public void should_return_Fizz_when_say_given_3() {
-        // given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        // when
-        String resultActual = fizzBuzz.say(3);
-
-        // then
-        assertEquals("Fizz", resultActual);
+        should_return_when_say_given_number(3, "Fizz");
     }
 
     @Test
     public void should_return_Buzz_when_say_given_5() {
-        // given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        // when
-        String resultActual = fizzBuzz.say(5);
-
-        // then
-        assertEquals("Buzz", resultActual);
+        should_return_when_say_given_number(5, "Buzz");
     }
 
     @Test
     public void should_return_FizzBuzz_when_say_given_multiple_of_three_and_five() {
-        // given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        // when
-        String resultActual = fizzBuzz.say(60);
-
-        // then
-        assertEquals("FizzBuzz", resultActual);
+        should_return_when_say_given_number(60, "FizzBuzz");
     }
 
     @Test
     public void should_return_Whizz_when_say_given_7() {
-        // given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        // when
-        String resultActual = fizzBuzz.say(7);
-
-        // then
-        assertEquals("Whizz", resultActual);
+        should_return_when_say_given_number(7, "Whizz");
     }
 
     @Test
     public void should_return_FizzWhizz_when_say_given_multiple_of_three_and_seven() {
-        // given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        // when
-        String resultActual = fizzBuzz.say(21);
-
-        // then
-        assertEquals("FizzWhizz", resultActual);
+        should_return_when_say_given_number(21, "FizzWhizz");
     }
 
     @Test
     public void should_return_BuzzWhizz_when_say_given_multiple_of_five_and_seven() {
-        // given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        // when
-        String resultActual = fizzBuzz.say(105);
-
-        // then
-        assertEquals("BuzzWhizz", resultActual);
+        should_return_when_say_given_number(105, "BuzzWhizz");
     }
 
     @Test
     public void should_return_Whizz_when_say_given_70() {
-        // given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        // when
-        String resultActual = fizzBuzz.say(70);
-
-        // then
-        assertEquals("Whizz", resultActual);
+        should_return_when_say_given_number(70, "Whizz");
     }
 
     @Test
     public void should_return_BuzzWhizz_when_say_given_multiple_of_three_and_five_and_seven() {
-        // given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        // when
-        String resultActual = fizzBuzz.say(210);
-
-        // then
-        assertEquals("FizzBuzzWhizz", resultActual);
+        should_return_when_say_given_number(210, "FizzBuzzWhizz");
     }
 
     @Test
     public void should_return_Fizz_when_say_given_13() {
-        // given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        // when
-        String resultActual = fizzBuzz.say(13);
-
-        // then
-        assertEquals("Fizz", resultActual);
+        should_return_when_say_given_number(13, "Fizz");
     }
 
     @Test
     public void should_return_BuzzWhizz_when_say_given_35() {
-        // given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        // when
-        String resultActual = fizzBuzz.say(35);
-
-        // then
-        assertEquals("BuzzWhizz", resultActual);
+        should_return_when_say_given_number(35, "BuzzWhizz");
     }
 
     @Test
     public void should_return_Fizz_when_say_given_30() {
-        // given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        // when
-        String resultActual = fizzBuzz.say(30);
-
-        // then
-        assertEquals("Fizz", resultActual);
+        should_return_when_say_given_number(30, "Fizz");
     }
 
     @Test
     public void should_return_Fizz_when_say_given_15() {
-        // given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        // when
-        String resultActual = fizzBuzz.say(15);
-
-        // then
-        assertEquals("Buzz", resultActual);
+        should_return_when_say_given_number(15, "Buzz");
     }
 
     @Test
     public void should_return_Fizz_when_say_given_75() {
+        should_return_when_say_given_number(75, "Fizz");
+    }
+
+    private void should_return_when_say_given_number(int number, String expected) {
         // given
         FizzBuzz fizzBuzz = new FizzBuzz();
 
         // when
-        String resultActual = fizzBuzz.say(75);
+        String resultActual = fizzBuzz.say(number);
 
         // then
-        assertEquals("Fizz", resultActual);
+        assertEquals(expected, resultActual);
     }
 }
