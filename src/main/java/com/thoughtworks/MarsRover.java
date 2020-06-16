afa8ce5522d67d671dec386445f11f68a11179c6
humenglin <humenglin@zyb.com.cn>
2020-06-16 13:14:28 +0800

    [refactor]:重构火星车action方法的返回值

--- src/main/java/com/thoughtworks/MarsRover.java
+++ src/main/java/com/thoughtworks/MarsRover.java
@@ -14,35 +14,29 @@ public class MarsRover {
         if (ActionCommandEnum.MOVE == actionCommand) {
             if (DirectionEnum.NORTH == this.marsPosition.getDirection()) {
                 this.marsPosition.setPositionY(this.marsPosition.getPositionY() + 1);
-                return this.marsPosition;
             }
             if (DirectionEnum.SOUTH == this.marsPosition.getDirection()) {
                 this.marsPosition.setPositionY(this.marsPosition.getPositionY() - 1);
-                return this.marsPosition;
             }
         }
 
         if (ActionCommandEnum.TURN_LEFT == actionCommand) {
             if (DirectionEnum.NORTH == this.marsPosition.getDirection()) {
                 this.marsPosition.setDirection(DirectionEnum.WEST);
-                return this.marsPosition;
             }
             if (DirectionEnum.SOUTH == this.marsPosition.getDirection()) {
                 this.marsPosition.setDirection(DirectionEnum.EAST);
-                return this.marsPosition;
             }
         }
 
         if (ActionCommandEnum.TURN_RIGHT == actionCommand) {
             if (DirectionEnum.NORTH == this.marsPosition.getDirection()) {
                 this.marsPosition.setDirection(DirectionEnum.EAST);
-                return this.marsPosition;
             }
             if (DirectionEnum.SOUTH == this.marsPosition.getDirection()) {
                 this.marsPosition.setDirection(DirectionEnum.WEST);
-                return this.marsPosition;
             }
         }
-        return null;
+        return this.marsPosition;
     }
 }
