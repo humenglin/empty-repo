0ae27cd44611946166540abcec1345d81cb34e94
humenglin <humenglin@zyb.com.cn>
2020-06-16 14:05:46 +0800

    [refactor]:重构测试方法，将公共的步骤抽成一个单独的方法

--- src/test/java/com/thoughtworks/MarsRoverTest.java
+++ src/test/java/com/thoughtworks/MarsRoverTest.java
@@ -14,157 +14,122 @@ public class MarsRoverTest {
     @Test
     public void should_move_north_one_step_when_receiveCommands_given_a_move_command_and_face_north() {
         MarsPosition marsPositionBefore = new MarsPosition(0, 0, DirectionEnum.NORTH);
-        List<ActionCommandEnum> actionCommands = new ArrayList<>();
         ActionCommandEnum actionCommand = ActionCommandEnum.MOVE;
-        actionCommands.add(actionCommand);
+        MarsPosition marsPositionExpected = new MarsPosition(0, 1, DirectionEnum.NORTH);
 
-        MarsRover marsRover = new MarsRover(marsPositionBefore);
-        MarsPosition marsPositionAfter = marsRover.receiveCommands(actionCommands);
-
-        assertThat(marsPositionAfter).isEqualToComparingFieldByField(new MarsPosition(0, 1, DirectionEnum.NORTH));
+        should_return_mars_position_when_action_by_single_command_given_a_init_position_and_a_command(marsPositionBefore, actionCommand, marsPositionExpected);
     }
 
     @Test
     public void should_turn_west_when_receiveCommands_given_a_turn_left_command_and_face_north() {
         MarsPosition marsPositionBefore = new MarsPosition(0, 0, DirectionEnum.NORTH);
-        List<ActionCommandEnum> actionCommands = new ArrayList<>();
         ActionCommandEnum actionCommand = ActionCommandEnum.TURN_LEFT;
-        actionCommands.add(actionCommand);
+        MarsPosition marsPositionExpected = new MarsPosition(0, 0, DirectionEnum.WEST);
 
-        MarsRover marsRover = new MarsRover(marsPositionBefore);
-        MarsPosition marsPositionAfter = marsRover.receiveCommands(actionCommands);
-
-        assertThat(marsPositionAfter).isEqualToComparingFieldByField(new MarsPosition(0, 0, DirectionEnum.WEST));
+        should_return_mars_position_when_action_by_single_command_given_a_init_position_and_a_command(marsPositionBefore, actionCommand, marsPositionExpected);
     }
 
     @Test
     public void should_turn_east_when_receiveCommands_given_a_turn_right_command_and_face_north() {
         MarsPosition marsPositionBefore = new MarsPosition(0, 0, DirectionEnum.NORTH);
-        List<ActionCommandEnum> actionCommands = new ArrayList<>();
         ActionCommandEnum actionCommand = ActionCommandEnum.TURN_RIGHT;
-        actionCommands.add(actionCommand);
+        MarsPosition marsPositionExpected = new MarsPosition(0, 0, DirectionEnum.EAST);
 
-        MarsRover marsRover = new MarsRover(marsPositionBefore);
-        MarsPosition marsPositionAfter = marsRover.receiveCommands(actionCommands);
-
-        assertThat(marsPositionAfter).isEqualToComparingFieldByField(new MarsPosition(0, 0, DirectionEnum.EAST));
+        should_return_mars_position_when_action_by_single_command_given_a_init_position_and_a_command(marsPositionBefore, actionCommand, marsPositionExpected);
     }
 
     @Test
     public void should_move_south_one_step_when_receiveCommands_given_a_move_command_and_face_south() {
         MarsPosition marsPositionBefore = new MarsPosition(0, 0, DirectionEnum.SOUTH);
-        List<ActionCommandEnum> actionCommands = new ArrayList<>();
         ActionCommandEnum actionCommand = ActionCommandEnum.MOVE;
-        actionCommands.add(actionCommand);
+        MarsPosition marsPositionExpected = new MarsPosition(0, -1, DirectionEnum.SOUTH);
 
-        MarsRover marsRover = new MarsRover(marsPositionBefore);
-        MarsPosition marsPositionAfter = marsRover.receiveCommands(actionCommands);
-
-        assertThat(marsPositionAfter).isEqualToComparingFieldByField(new MarsPosition(0, -1, DirectionEnum.SOUTH));
+        should_return_mars_position_when_action_by_single_command_given_a_init_position_and_a_command(marsPositionBefore, actionCommand, marsPositionExpected);
     }
 
     @Test
     public void should_turn_east_when_receiveCommands_given_a_turn_left_command_and_face_south() {
         MarsPosition marsPositionBefore = new MarsPosition(0, 0, DirectionEnum.SOUTH);
-        List<ActionCommandEnum> actionCommands = new ArrayList<>();
         ActionCommandEnum actionCommand = ActionCommandEnum.TURN_LEFT;
-        actionCommands.add(actionCommand);
+        MarsPosition marsPositionExpected = new MarsPosition(0, 0, DirectionEnum.EAST);
 
-        MarsRover marsRover = new MarsRover(marsPositionBefore);
-        MarsPosition marsPositionAfter = marsRover.receiveCommands(actionCommands);
-
-        assertThat(marsPositionAfter).isEqualToComparingFieldByField(new MarsPosition(0, 0, DirectionEnum.EAST));
+        should_return_mars_position_when_action_by_single_command_given_a_init_position_and_a_command(marsPositionBefore, actionCommand, marsPositionExpected);
     }
 
     @Test
     public void should_turn_west_when_receiveCommands_given_a_turn_right_command_and_face_south() {
         MarsPosition marsPositionBefore = new MarsPosition(0, 0, DirectionEnum.SOUTH);
-        List<ActionCommandEnum> actionCommands = new ArrayList<>();
         ActionCommandEnum actionCommand = ActionCommandEnum.TURN_RIGHT;
-        actionCommands.add(actionCommand);
+        MarsPosition marsPositionExpected = new MarsPosition(0, 0, DirectionEnum.WEST);
 
-        MarsRover marsRover = new MarsRover(marsPositionBefore);
-        MarsPosition marsPositionAfter = marsRover.receiveCommands(actionCommands);
-
-        assertThat(marsPositionAfter).isEqualToComparingFieldByField(new MarsPosition(0, 0, DirectionEnum.WEST));
+        should_return_mars_position_when_action_by_single_command_given_a_init_position_and_a_command(marsPositionBefore, actionCommand, marsPositionExpected);
     }
 
     @Test
     public void should_move_east_one_step_when_receiveCommands_given_a_move_command_and_face_east() {
         MarsPosition marsPositionBefore = new MarsPosition(0, 0, DirectionEnum.EAST);
-        List<ActionCommandEnum> actionCommands = new ArrayList<>();
         ActionCommandEnum actionCommand = ActionCommandEnum.MOVE;
-        actionCommands.add(actionCommand);
+        MarsPosition marsPositionExpected = new MarsPosition(1, 0, DirectionEnum.EAST);
 
-        MarsRover marsRover = new MarsRover(marsPositionBefore);
-        MarsPosition marsPositionAfter = marsRover.receiveCommands(actionCommands);
-
-        assertThat(marsPositionAfter).isEqualToComparingFieldByField(new MarsPosition(1, 0, DirectionEnum.EAST));
+        should_return_mars_position_when_action_by_single_command_given_a_init_position_and_a_command(marsPositionBefore, actionCommand, marsPositionExpected);
     }
 
     @Test
     public void should_turn_north_when_receiveCommands_given_a_turn_left_command_and_face_east() {
         MarsPosition marsPositionBefore = new MarsPosition(0, 0, DirectionEnum.EAST);
-        List<ActionCommandEnum> actionCommands = new ArrayList<>();
         ActionCommandEnum actionCommand = ActionCommandEnum.TURN_LEFT;
-        actionCommands.add(actionCommand);
+        MarsPosition marsPositionExpected = new MarsPosition(0, 0, DirectionEnum.NORTH);
 
-        MarsRover marsRover = new MarsRover(marsPositionBefore);
-        MarsPosition marsPositionAfter = marsRover.receiveCommands(actionCommands);
-
-        assertThat(marsPositionAfter).isEqualToComparingFieldByField(new MarsPosition(0, 0, DirectionEnum.NORTH));
+        should_return_mars_position_when_action_by_single_command_given_a_init_position_and_a_command(marsPositionBefore, actionCommand, marsPositionExpected);
     }
 
     @Test
     public void should_turn_south_when_receiveCommands_given_a_turn_right_command_and_face_east() {
         MarsPosition marsPositionBefore = new MarsPosition(0, 0, DirectionEnum.EAST);
-        List<ActionCommandEnum> actionCommands = new ArrayList<>();
         ActionCommandEnum actionCommand = ActionCommandEnum.TURN_RIGHT;
-        actionCommands.add(actionCommand);
+        MarsPosition marsPositionExpected = new MarsPosition(0, 0, DirectionEnum.SOUTH);
 
-        MarsRover marsRover = new MarsRover(marsPositionBefore);
-        MarsPosition marsPositionAfter = marsRover.receiveCommands(actionCommands);
-
-        assertThat(marsPositionAfter).isEqualToComparingFieldByField(new MarsPosition(0, 0, DirectionEnum.SOUTH));
+        should_return_mars_position_when_action_by_single_command_given_a_init_position_and_a_command(marsPositionBefore, actionCommand, marsPositionExpected);
     }
 
     @Test
     public void should_move_west_one_step_when_receiveCommands_given_a_move_command_and_face_west() {
         MarsPosition marsPositionBefore = new MarsPosition(0, 0, DirectionEnum.WEST);
-        List<ActionCommandEnum> actionCommands = new ArrayList<>();
         ActionCommandEnum actionCommand = ActionCommandEnum.MOVE;
-        actionCommands.add(actionCommand);
+        MarsPosition marsPositionExpected = new MarsPosition(-1, 0, DirectionEnum.WEST);
 
-        MarsRover marsRover = new MarsRover(marsPositionBefore);
-        MarsPosition marsPositionAfter = marsRover.receiveCommands(actionCommands);
-
-        assertThat(marsPositionAfter).isEqualToComparingFieldByField(new MarsPosition(-1, 0, DirectionEnum.WEST));
+        should_return_mars_position_when_action_by_single_command_given_a_init_position_and_a_command(marsPositionBefore, actionCommand, marsPositionExpected);
     }
 
     @Test
     public void should_turn_south_when_receiveCommands_given_a_turn_left_command_and_face_west() {
         MarsPosition marsPositionBefore = new MarsPosition(0, 0, DirectionEnum.WEST);
-        List<ActionCommandEnum> actionCommands = new ArrayList<>();
         ActionCommandEnum actionCommand = ActionCommandEnum.TURN_LEFT;
-        actionCommands.add(actionCommand);
+        MarsPosition marsPositionExpected = new MarsPosition(0, 0, DirectionEnum.SOUTH);
 
-        MarsRover marsRover = new MarsRover(marsPositionBefore);
-        MarsPosition marsPositionAfter = marsRover.receiveCommands(actionCommands);
-
-        assertThat(marsPositionAfter).isEqualToComparingFieldByField(new MarsPosition(0, 0, DirectionEnum.SOUTH));
+        should_return_mars_position_when_action_by_single_command_given_a_init_position_and_a_command(marsPositionBefore, actionCommand, marsPositionExpected);
     }
 
     @Test
     public void should_turn_north_when_receiveCommands_given_a_turn_right_command_and_face_west() {
         MarsPosition marsPositionBefore = new MarsPosition(0, 0, DirectionEnum.WEST);
-        List<ActionCommandEnum> actionCommands = new ArrayList<>();
         ActionCommandEnum actionCommand = ActionCommandEnum.TURN_RIGHT;
+        MarsPosition marsPositionExpected = new MarsPosition(0, 0, DirectionEnum.NORTH);
+
+        should_return_mars_position_when_action_by_single_command_given_a_init_position_and_a_command(marsPositionBefore, actionCommand, marsPositionExpected);
+    }
+
+    private void should_return_mars_position_when_action_by_single_command_given_a_init_position_and_a_command(MarsPosition marsPositionBefore, ActionCommandEnum actionCommand, MarsPosition marsPositionExpected) {
+        // given
+        MarsRover marsRover = new MarsRover(marsPositionBefore);
+        List<ActionCommandEnum> actionCommands = new ArrayList<>();
         actionCommands.add(actionCommand);
 
-        MarsRover marsRover = new MarsRover(marsPositionBefore);
+        // when
         MarsPosition marsPositionAfter = marsRover.receiveCommands(actionCommands);
 
-        assertThat(marsPositionAfter).isEqualToComparingFieldByField(new MarsPosition(0, 0, DirectionEnum.NORTH));
+        // then
+        assertThat(marsPositionAfter).isEqualToComparingFieldByField(marsPositionExpected);
     }
 
     @Test
