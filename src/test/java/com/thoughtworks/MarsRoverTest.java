908ae214a0f487704f10de15c3e786a81d88a7ab
humenglin <humenglin@zyb.com.cn>
2020-06-16 10:30:06 +0800

    [test]:提交火星车需求的测试案例名称

---
+++ src/test/java/com/thoughtworks/MarsRoverTest.java
@@ -0,0 +1,70 @@
+package com.thoughtworks;
+
+import org.junit.Test;
+
+public class MarsRoverTest {
+
+    @Test
+    public void should_move_north_one_step_when_action_given_a_move_command_and_face_north() {
+    }
+
+    @Test
+    public void should_turn_west_when_action_given_a_turn_left_command_and_face_north() {
+
+    }
+
+    @Test
+    public void should_turn_east_when_action_given_a_turn_right_command_and_face_north() {
+
+    }
+
+    @Test
+    public void should_move_south_one_step_when_action_given_a_move_command_and_face_south() {
+
+    }
+
+    @Test
+    public void should_turn_east_when_action_given_a_turn_left_command_and_face_south() {
+
+    }
+
+    @Test
+    public void should_turn_west_when_action_given_a_turn_right_command_and_face_south() {
+
+    }
+
+    @Test
+    public void should_move_east_one_step_when_action_given_a_move_command_and_face_east() {
+
+    }
+
+    @Test
+    public void should_turn_north_when_action_given_a_turn_left_command_and_face_east() {
+
+    }
+
+    @Test
+    public void should_turn_south_when_action_given_a_turn_right_command_and_face_east() {
+
+    }
+
+    @Test
+    public void should_move_west_one_step_when_action_given_a_move_command_and_face_west() {
+
+    }
+
+    @Test
+    public void should_turn_south_when_action_given_a_turn_left_command_and_face_west() {
+
+    }
+
+    @Test
+    public void should_turn_north_when_action_given_a_turn_right_command_and_face_west() {
+
+    }
+
+    @Test
+    public void should_move_to_some_position_when_action_given_a_series_commands() {
+
+    }
+}
